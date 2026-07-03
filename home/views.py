67da9fa4cf7190a49b1d2b38b09baae6396e99cb
shashi khadka <shashikhadka84@gmail.com>
2026-07-03 16:08:42 +0545

    base extension

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from meal.models import Meal, MealIngredients, MealPlan, MealPlanItems, Ingredient
+from meals.models import Meal, MealIngredients, MealPlan, MealPlanItems, Ingredient
+from django.db.models import Count
 
 # Create your views here.
 
@@ -10,14 +11,15 @@ def dashboard(request):
     meal_plan = MealPlan.objects.all()
     meal_plan_items = MealPlanItems.objects.all()
     ingredient = Ingredient.objects.all()
-
-
+    meal_type = meal.values_list('meal_type').annotate(count = Count('meal_type')).order_by('meal_type')
     context = {
-    "cards": [
-        {"title": "Meals", "count": meal.count()},
-        {"title": "Meal Ingredients", "count": meal_ingredient.count()},
-        {"title": "Meal Plans", "count": meal_plan.count()},
-    ]
-}
+        "cards": [
+            {"title": "Meals", "count": meal.count()},
+            {"title": "Meal Ingredients", "count": meal_ingredient.count()},
+            {"title": "Meal Plans", "count": meal_plan.count()},
+            {"title":"Ingredient", "count":ingredient.count()},
+        ],
+        "meal_type":[{"title":i[0], "count":i[1]} for i in meal_type],
+    }
 
-    return render(request,'home/base.html',context)
+    return render(request, "home/base.html", context)
